e0831aa6334bb609cab74383680be09bb78ce674
marleigh <volcana@gmail.com>
2022-12-23 15:00:09 -0800

    very basic fetch call + display on page

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const model = require('./connection.js');
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*'); //this is probably bad!
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,12 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 
-function App() {
+
+const App:React.FC = () => {
+  const [spells, setSpells] = useState<any>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001')
+    .then(response =>  response.json())
+    .then(data => setSpells(data))
+  }, []);
+
+  const spellMap = spells.map(spell => (
+    <div className="spell-box">
+      <ul className="spell-list">
+        <li>
+          Name: {spell.name}
+        </li>
+        <li>
+          School: {spell.school}
+        </li>
+        <li>
+          Description: {spell.description}
+        </li>
+        <li>
+          Range: {spell.range}
+        </li>
+      </ul>
+
+    </div>
+  ))
 
   return (
-    <div className="App">
-     
-    </div>
+    <>
+     {spellMap}
+    </>
   )
 }
 
-export default App
+export default App;
