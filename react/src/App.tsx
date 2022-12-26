acabf747c759be121041b0d89f451698d3b56467
marleigh <volcana@gmail.com>
2022-12-26 14:18:26 -0800

    adding a few other elements

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -7,7 +7,11 @@ interface spellTypes {
   subschool: string,
   descriptor: string,
   saving_throws: boolean,
-  school: string
+  school: string,
+  description: string,
+  range: string,
+  area: string,
+  effect: string
 }
 
 const App:React.FC = () => {
@@ -19,6 +23,10 @@ const App:React.FC = () => {
     .then(data => setSpells(data))
   }, []);
 
+
+  // name,school,subschool,descriptor,spell_level,casting_time,range,area,effect,targets,duration,
+//dismissible,shapeable,saving_throw,description,description_formatted,full_text,domain,short_description,mythic_text,mythic
+
   const spellMap = spells.map((spell: spellTypes) => (
     <div className="spell-box">
       <ul className="spell-list">
@@ -29,11 +37,17 @@ const App:React.FC = () => {
           School: {spell.school}
         </li>
         <li>
-          Description: {spell.descriptor}
-        </li>
-        <li>
           Subschool: {spell.subschool}
         </li>
+        <li>
+          Description: {spell.description}
+        </li>
+        <li>
+         Range: {spell.range} <br/>
+         Area: {spell.area} <br/>
+         Effect: {spell.effect}
+        </li>
+
       </ul>
 
     </div>
