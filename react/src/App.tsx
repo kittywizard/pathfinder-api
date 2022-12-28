6e5722c1e4a866e4fa0ffb7e42e5596a3e7c4413
marleigh <volcana@gmail.com>
2022-12-28 14:08:07 -0800

    adding a loop to limit results for now

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -23,35 +23,42 @@ const App:React.FC = () => {
     .then(data => setSpells(data))
   }, []);
 
-
-  // name,school,subschool,descriptor,spell_level,casting_time,range,area,effect,targets,duration,
+// spell API names:
+// name,school,subschool,descriptor,spell_level,casting_time,range,area,effect,targets,duration,
 //dismissible,shapeable,saving_throw,description,description_formatted,full_text,domain,short_description,mythic_text,mythic
 
-  const spellMap = spells.map((spell: spellTypes) => (
-    <div className="spell-box">
-      <ul className="spell-list">
-        <li>
-          Name: {spell.name}
-        </li>
-        <li>
-          School: {spell.school}
-        </li>
-        <li>
-          Subschool: {spell.subschool}
-        </li>
-        <li>
-          Description: {spell.description}
-        </li>
-        <li>
-         Range: {spell.range} <br/>
-         Area: {spell.area} <br/>
-         Effect: {spell.effect}
-        </li>
+  const spellMap = spells.map((spell: spellTypes) => {
+    for(let i=0; i < 20; i++) { //basically to not kill my page - need a way to sort / limit later
+      return (
+        <div className="spell-box">
+          <ul className="spell-list">
+            <li>
+              Name: {spell.name}
+            </li>
+            <li>
+              School: {spell.school}
+            </li>
+            <li>
+              Subschool: {spell.subschool}
+            </li>
+            <li>
+              Description: {spell.description}
+            </li>
+            <li>
+            Range: {spell.range} <br/>
+            Area: {spell.area} <br/>
+            Effect: {spell.effect}
+            </li>
 
-      </ul>
+          </ul>
 
-    </div>
-  ))
+        </div>
+      )
+
+    }
+
+
+  })
 
   return (
     <>
